83626bc52770f46f75737c3401f6e99991b72d2e
sgs-divyashri <divyashri.b@sgstechsolutions.com>
2025-11-25 06:46:02 +0530

    update

--- Week-3/test/src/server.ts
+++ Week-3/test/src/server.ts
@@ -8,12 +8,6 @@ const init = async () => {
         const server = Hapi.server({
             port: 3000,
             host: 'localhost',
-            routes: {
-                cors: {
-                    origin: ['*'],
-                    credentials: true,
-                },
-            },
         });
 
         // Register JWT plugin ONLY HERE
