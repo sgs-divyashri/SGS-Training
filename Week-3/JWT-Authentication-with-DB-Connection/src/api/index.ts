bd020f64a093df51772630cd3e2e2aef247a9ae8
sgs-divyashri <divyashri.b@sgstechsolutions.com>
2025-11-30 10:29:35 +0530

    update

--- Week-3/JWT-Authentication-with-DB-Connection/src/api/index.ts
+++
@@ -1,4 +0,0 @@
-import { taskRoutes } from "./tasks/index";
-import { userRoutes } from "./users/index";
-
-export default [...userRoutes, ...taskRoutes]
--- Week-3/JWT-Authentication-with-DB-Connection/src/sequelize/sequelize.ts
+++
@@ -1,6 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-export const sequelize = new Sequelize('projectdb', 'postgres', 'Divya0301', {  // db-name, username, pass, options
-    host: 'localhost',
-    dialect: 'postgres',  // sql    
-})
