29e63dd125a08d3a8df17dc04cc9fd075b2e34d5
sgs-divyashri <divyashri.b@sgstechsolutions.com>
2025-12-08 12:09:01 +0530

    initialize and define sequelize object

--- Week-3/JWT-Authentication-with-DB-Connection/src/sequelize/sequelize.ts
+++ Week-3/JWT-Authentication-with-DB-Connection/src/sequelize/sequelize.ts
@@ -2,6 +2,6 @@ import { Sequelize } from 'sequelize';
 
 export const sequelize = new Sequelize('projectdb', 'postgres', 'Divya0301', {  // db-name, username, pass, options
     host: 'localhost',
-    dialect: 'postgres',  // sql   
+    dialect: 'postgres',    
     logging: false 
 })
