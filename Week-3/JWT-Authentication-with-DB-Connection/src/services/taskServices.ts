eeed13a044bb607bc20c6246305b2c8af48c1d11
sgs-divyashri <divyashri.b@sgstechsolutions.com>
2025-12-08 11:20:28 +0530

    task services

--- Week-3/JWT-Authentication-with-DB-Connection/src/services/taskServices.ts
+++ Week-3/JWT-Authentication-with-DB-Connection/src/services/taskServices.ts
@@ -2,17 +2,6 @@ import { Model } from "sequelize";
 import { taskRepository } from "../repository/taskRepo";
 import { TaskPayload } from "../models/taskTableDefinition";
 
-// export interface TaskPayload {
-//   taskID: string;
-//   taskName: string;
-//   description: string;
-//   status: string;
-//   createdBy: number;
-//   createdAt: string;
-//   updatedAt: string;
-//   isActive: boolean;
-// }
-
 export const taskServices = {
   createTask: async (payload: Pick<TaskPayload, "taskName" | "description" | "createdBy">) => {
     const user = await taskRepository.createTask(payload)
