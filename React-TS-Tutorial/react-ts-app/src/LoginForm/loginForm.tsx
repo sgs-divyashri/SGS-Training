c54d350329a78fa7c16e89a540f4dfe7a8740c75
sgs-divyashri <divyashri.b@sgstechsolutions.com>
2025-12-10 19:05:20 +0530

    update

--- React-TS-Tutorial/react-ts-app/src/LoginForm/loginForm.tsx
+++ React-TS-Tutorial/react-ts-app/src/LoginForm/loginForm.tsx
@@ -1,11 +1,8 @@
-// import { useState } from "react";
-
 import FieldInputs from "./fieldInputs";
 
 export default function LoginForm() {
     return (
         <div>
-            <h2>Login Form</h2>
             <FieldInputs />
         </div>
     )
