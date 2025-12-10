13f286af2503ec0a3a542abefa667fa0eba183e1
sgs-divyashri <divyashri.b@sgstechsolutions.com>
2025-12-10 19:04:42 +0530

    Login Page - h1, input fields, button, eventListener to handleSubmit

--- React-TS-Tutorial/react-ts-app/src/LoginForm/fieldInputs.tsx
+++ React-TS-Tutorial/react-ts-app/src/LoginForm/fieldInputs.tsx
@@ -5,15 +5,20 @@ export default function FieldInputs() {
     const [password, setPassword] = useState<string>("")
     const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
         event.preventDefault();
+        alert("Form submitted...")
         console.log("Email: ", email)
         console.log("Password: ", password)
     }
     return (
         <form onSubmit={handleSubmit}>
-            <div>
-                <input type="email" name="email" placeholder="Enter email ID" required value={email} onChange={(e) => setEmail(e.target.value)}/> <br />
-                <input type="password" name="password" placeholder="Enter Password" required value={password} onChange={(e) => setPassword(e.target.value)}/> <br />
-                <button type="submit">Login</button>
+            <div className="body">
+                <div className="card">
+                    <h1 className="card-title">Login Form</h1>
+                    <input type="email" className="input-field" name="email" placeholder="Enter email ID" required value={email} onChange={(e) => setEmail(e.target.value)} /> <br />
+                    <input type="password" className="input-field" name="password" placeholder="Enter Password" required value={password} onChange={(e) => setPassword(e.target.value)} /> <br />
+                    <button type="submit" className="login-btn">Login</button>
+                    <button type="reset">Res</button>
+                </div>
             </div>
         </form>
     )
