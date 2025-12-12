f587f1ba1ab66d5cec19c4cd6569c6126b4a50b0
sgs-divyashri <divyashri.b@sgstechsolutions.com>
2025-12-12 17:25:58 +0530

    used props for passing dynamic data

--- React-TS-Tutorial/react-ts-app/src/RegistrationForm/loginInputs.tsx
+++ React-TS-Tutorial/react-ts-app/src/RegistrationForm/loginInputs.tsx
@@ -1,28 +1,30 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom";
+import { FieldConfig } from "./loginForm";
+import { UserPayload } from "./registerForm";
 
-export default function LoginInputs() {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState<string>("")
-    const [password, setPassword] = useState<string>("")
-    const handleClick = () => {
-        navigate("/users");
-    }
+interface RegisterInputsProps {
+    fields: FieldConfig[];
+    values: Pick<UserPayload, "email" | "password">;
+    onChange: (name: keyof Pick<UserPayload, "email" | "password">, raw: string) => void;
+}
+
+export default function LoginInputs({ fields, values, onChange }: RegisterInputsProps) {
     return (
-        <form>
-            <div className="flex items-center gap-4">
-                <label className="font-semibold w-24">Email: </label>
-                <input type="email" className="p-2 m-3 border-2 w-full mx-2 rounded-xl" placeholder="Enter email ID" required value={email} onChange={(e) => setEmail(e.target.value)} />
-            </div>
-            <div className="flex items-center gap-4">
-                <label className="font-semibold w-24">Password: </label>
-                <input type="password" className="p-2 m-3 border-2 w-full mx-2 rounded-xl" placeholder="Enter password" required value={password} onChange={(e) => setPassword(e.target.value)} />
-            </div>
-            <div className="flex justify-center gap-3 m-3">
-                <button type="submit" className="text-white bg-pink-400 border-2 px-6 py-3 rounded-xl hover:bg-pink-600" onClick={() => handleClick()}>
-                    Login
-                </button>
-            </div>
-        </form>
-    )
+        <div>
+            {fields.map((f) => (
+                <div key={String(f.name)} className="flex items-center gap-4">
+                    <label className="font-semibold w-24">{f.label}: </label>
+                    <input
+                        type={f.type}
+                        className="p-2 m-3 border-2 w-full mx-2 rounded-xl"
+                        placeholder={f.placeholder}
+                        required
+                        value={values[f.name] === undefined ? "" : (values[f.name] as string)}
+                        onChange={(e) => onChange(f.name, e.target.value)}
+                    />
+                </div>
+            ))}
+        </div>
+    );
 }
