03336a01e94cfccc7c2717573241e94fc0dd9ba1
sgs-divyashri <divyashri.b@sgstechsolutions.com>
2025-12-15 16:32:42 +0530

    The main file to wrap up all the JSX elements and use separate Route paths for navigation

--- React-TS-Tutorial/react-ts-app/src/App.tsx
+++ React-TS-Tutorial/react-ts-app/src/App.tsx
@@ -1,15 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginForm from "./RegistrationForm/loginForm";
+import LoginForm from "./Users/loginForm";
 import './index.css';
-import RegisterForm from './RegistrationForm/registerForm';
-import UsersList from "./RegistrationForm/registeredUsersList";
+import RegisterForm from './Users/registerForm';
+import UsersList from "./Users/registeredUsersList";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route></Route>
-        <Route path="/" element={<RegisterForm />} ></Route>
+        <Route path="" element={<RegisterForm />} ></Route>
         <Route path="/login" element={<LoginForm />} ></Route>
         <Route path="/users" element={<UsersList/>} ></Route>
       </Routes>
