2802c3eef3692278f87a8a081e60a156116c118c
sgs-divyashri <divyashri.b@sgstechsolutions.com>
2025-12-17 16:38:47 +0530

    functionality for all user endpoints

--- Week-3/JWT-Authentication-with-DB-Connection/src/repository/userRepo.ts
+++ Week-3/JWT-Authentication-with-DB-Connection/src/repository/userRepo.ts
@@ -3,13 +3,13 @@ import { passwordServices } from "../services/passwordservices";
 import { UserPayload } from "../models/userTableDefinition";
 
 export const userRepository = {
-    createUser: async (payload: Pick<UserPayload, "name"|"email"|"password"|"age">): Promise<User> => {
+    createUser: async (payload: Pick<UserPayload, "name" | "email" | "password" | "age">): Promise<User> => {
         const newUser = await User.create({ ...payload, password: passwordServices.hashPassword(payload.password!) });
         return newUser
     },
 
     loginUser: async (loginData: Pick<UserPayload, "email" | "password">): Promise<User | null> => {
-        const user = await User.findOne({ where: { email: loginData.email, isActive: true }, attributes: {include: ["password"]} });
+        const user = await User.findOne({ where: { email: loginData.email, isActive: true }, attributes: { include: ["password"] } });
         if (!user) return null;
 
         // Access password safely
@@ -17,24 +17,24 @@ export const userRepository = {
         // console.log("Hashed password for login user: ",hashedPassword)
         const isValidPassword = passwordServices.verifyPassword(loginData.password, hashedPassword);
         if (!isValidPassword) return null;
-        
+
         return user;
     },
 
     getAllUsers: (): Promise<User[]> => {
-        const users = User.findAll({ where: { isActive: true } });
+        const users = User.findAll();
         return users
     },
 
     getSpecificUser: async (id: number): Promise<User | null> => {
-        const user = await User.findOne({ where: { userId: id, isActive: true } })
+        const user = await User.findOne({ where: { userId: id } })
         return user
     },
 
     fullUpdateUser: async (id: number, payload: Pick<UserPayload, "name" | "email" | "password" | "age">): Promise<User | null | undefined> => {
         const [rowsUpdated, [updatedUser]] = await User.update({ ...payload, password: passwordServices.hashPassword(payload.password) }, { where: { userId: id, isActive: true }, returning: true })
         if (rowsUpdated === 0) {
-            return null; 
+            return null;
         }
 
         return updatedUser
@@ -53,11 +53,25 @@ export const userRepository = {
         return user.get();
     },
 
-    softDeleteUser: async (id: number): Promise<number> => {
-        const [affectedRows] = await User.update({ isActive: false }, { where: { userId: id, isActive: true } });
-        if (affectedRows === 0) {
+    // restoreUser: async (id: number): Promise<User | null> => {
+    //     const [affectedRows] = await User.update({ isActive: true }, { where: { userId: id, isActive: false } });
+    //     if (affectedRows === 0) {
+    //         throw new Error(`User with ID ${id} not found or already restored`);
+    //     };
+    //     return await User.findOne({ where: { userId: id } });
+    //     // return id
+    // },
+
+    toggleUser: async (id: number): Promise<User | null> => {
+        const user = await User.findOne({ where: { userId: id } });
+        if (!user) throw new Error(`User with ID ${id} not found`);
+
+        const nextIsActive = !Boolean(user.isActive);
+
+        const [activeRows] = await User.update({ isActive: nextIsActive }, { where: { userId: id } });
+        if (activeRows === 0) {
             throw new Error(`User with ID ${id} not found or already deleted`);
         }
-        return id;
+        return await User.findOne({ where: { userId: id } });
     }
 }
