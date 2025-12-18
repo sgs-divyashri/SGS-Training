a7a65956ab9983049d2974e444f20aab0c8f9a46
sgs-divyashri <divyashri.b@sgstechsolutions.com>
2025-12-18 18:24:49 +0530

    get a specific task from UI

---
+++ React-TS-Tutorial/react-ts-app/src/Tasks/getSpecificTask.tsx
@@ -0,0 +1,124 @@
+import { TaskPayload } from "./createTask";
+import { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { api } from "../Users/axiosClient";
+
+export default function SpecificTask() {
+    const [rows, setRows] = useState<TaskPayload[]>([]);
+    const { id } = useParams<{ id: string }>()
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!id) return;
+        const fetchUsers = async () => {
+            try {
+                const res = await api.get(`/tasks/${id}`);
+
+                const user = res.data.task ?? res.data;
+                // Ensure it's shaped like UserPayload and wrap as array
+                return setRows(user ? [user] : []);
+            }
+            catch (err: any) {
+                const msg =
+                    err.response?.data?.message ||
+                    err.message ||
+                    "Failed to fetch users.";
+            }
+        }
+        fetchUsers()
+    }, [id]);
+
+    const toggleActive = async (taskId: string) => {
+        setRows(prev =>
+            prev.map(row =>
+                row.taskId === taskId ? { ...row, isActive: !row.isActive } : row
+            )
+        );
+        try {
+            // call the correct backend endpoint
+            const res = await api.patch(`/users/toggle/${taskId}`)       // restore (reactivate)
+
+            // if backend returns updated user, sync with server truth
+            const updated = res.data?.user;
+            if (updated && typeof updated.isActive === "boolean") {
+                setRows((prev) =>
+                    prev.map((row) =>
+                        row.taskId === taskId ? { ...row, isActive: !!updated.isActive } : row
+                    )
+                );
+            }
+        } catch (err: any) {
+            setRows((prev) =>
+                prev.map((row) =>
+                    row.taskId === taskId ? { ...row, isActive: row.isActive } : row
+                )
+            );
+            console.error(
+                "Failed to update user.",
+                err?.response?.data?.message || err?.message || err
+            );
+
+        }
+    };
+
+    const editButton = (id: string) => navigate(`/users/f_update/${id}`);
+
+    const logout = () => {
+        navigate("/")
+    }
+
+    const handleClick = () => {
+        navigate(-1);
+    }
+
+    return (
+        <div className="bg-pink-200 mx-auto my-40 rounded-xl shadow p-5 border w-5xl">
+            <h2 className="font-semibold mb-4 text-center text-lg">Single User</h2>
+            <div className="overflow-x-auto">
+                <table className="min-w-full border border-gray-300">
+                    <thead className="bg-gray-100">
+                        <tr>
+                            <th className="border p-2 text-sm text-center">User ID</th>
+                            <th className="border p-2 text-sm text-center">Name</th>
+                            <th className="border p-2 text-sm text-center">Email</th>
+                            {/* <th className="border p-2 text-sm text-center">Password</th> */}
+                            <th className="border p-2 text-sm text-center">Age</th>
+                            <th className="border p-2 text-sm text-center">IsActive</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {rows.map((r) => (
+                            <tr key={r.taskId} className="bg-white">
+                                <td className="border p-2">{r.taskId}</td>
+                                <td className="border p-2">{r.taskName}</td>
+                                <td className="border p-2">{r.description}</td>
+                                <td className="border p-2">{r.status}</td>
+                                <td className="border p-2 text-center">
+                                    <label className="inline-flex items-center cursor-pointer select-none">
+                                        <input type="checkbox" checked={r.isActive} onChange={() => toggleActive(r.taskId)} className="sr-only" />
+                                        <div className={`relative w-16 h-8 rounded-full transition-colors duration-200 ${!!r.isActive ? "bg-green-500" : "bg-gray-300"} flex items-center`}>
+                                            <span className={`absolute top-1 left-1 w-6 h-6 rounded-full bg-white shadow transition-transform duration-200 ${!!r.isActive ? "translate-x-8" : "translate-x-0"}`} />
+                                        </div>
+                                    </label>
+                                </td>
+                                <div className="bg-pink-200 p-2 text-center">
+                                    <button type="button" onClick={() => editButton(r.taskId)} className="text-white bg-pink-400 border-2 px-3 py-2 rounded-xl hover:bg-pink-600">
+                                        Edit
+                                    </button>
+                                </div>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            <div className="flex justify-end">
+                <button type="button" onClick={handleClick} className="text-white bg-blue-300 border-2 my-4 px-4 py-3 rounded-xl hover:bg-blue-400">
+                    Go Back
+                </button>
+                <button type="submit" className="text-white bg-blue-300 border-2 m-4 px-4 py-3 rounded-xl hover:bg-blue-400" onClick={() => logout()}>
+                    Logout
+                </button>
+            </div>
+        </div>
+    )
+}
