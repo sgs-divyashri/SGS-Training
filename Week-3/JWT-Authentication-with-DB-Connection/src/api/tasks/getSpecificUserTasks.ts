fd50733b4b2fe8f9794baa5f4b013911b8c4bc50
sgs-divyashri <divyashri.b@sgstechsolutions.com>
2025-12-19 21:56:04 +0530

    get specific user's tasks controller

--- Week-3/JWT-Authentication-with-DB-Connection/src/api/tasks/getSpecificUserTasks.ts
+++ Week-3/JWT-Authentication-with-DB-Connection/src/api/tasks/getSpecificUserTasks.ts
@@ -1,18 +1,21 @@
 import { Request, ResponseObject, ResponseToolkit } from "@hapi/hapi";
 import { taskServices } from "../../services/taskServices";
+import { sequelize } from "../../sequelize/sequelize";
+import { validateEmail } from "../users/emailValidation";
+import { userServices } from "../../services/userServices";
 
 export const getSpecificUserTaskHandler = async (request: Request, h: ResponseToolkit): Promise<ResponseObject> => {
     try {
-        const userID = Number(request.params.id);
+        const { userId, email } = request.auth.credentials as { userId?: number; email?: string };
 
-        const userTasks = await taskServices.getSpecificUserTasks(userID);
+        const userTasks = await taskServices.getSpecificUserTasks(Number(userId));
         if (!userTasks) {
             return h.response({ error: "No tasks found for this user" }).code(404);
         }
-
+        
         return h.response({
             message: "Retrieved specific user's tasks successfully",
-            task: userTasks,
+            tasks: userTasks,
         }).code(200);
 
     } catch (err: any) {
